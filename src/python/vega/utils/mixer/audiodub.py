672f4f6988f24eb25f8fc9469a0bba3af1bae78c
biruk-ay <birukindustry@gmail.com>
2023-10-17 17:54:19 +0300

    mixed

--- src/python/vega/utils/mixer/audiodub.py
+++ src/python/vega/utils/mixer/audiodub.py
@@ -1,12 +1,31 @@
 from utils.mixer.mixer import Mixer
-from pydub import AudioSegment
-
+from pydub import AudioSegment, effects
+import librosa
+import noisereduce as nr
+import soundfile as sf
 
 class AudiodubMixer(Mixer):
 
 	def _mix(self, instrumental_path: str, vocal_path: str, output_path: str):
-		instrumental = AudioSegment.from_wav(instrumental_path)
-		vocal = AudioSegment.from_wav(vocal_path).apply_gain(-10)
+		instrumental = AudiodubMixer._normalize(instrumental_path)
+		vocal = AudiodubMixer._normalize(AudiodubMixer._reduce_noise(vocal_path))
 
 		output = instrumental.overlay(vocal, position=2000, gain_during_overlay=0)
 		output.export(output_path, format="wav")
+
+	def _reduce_noise(self, path):
+		audio, sr = librosa.load(path)
+		reduced_audio = nr.reduce_noise(y=audio, sr=sr)
+		output = Mixer._generate_filepath()
+		sf.write(output, reduced_audio, sr)
+		return output
+
+	def _normalize(self, path):
+		raw_audio = AudioSegment.from_file(path)
+		normalized_audio = effects.normalize(raw_audio)
+		output = Mixer._generate_filepath()
+		try:
+			normalized_audio.export(output)
+		except:
+			sf.write(output, normalized_audio)
+		return output
--- test/utils/mixer_test.py
+++ test/utils/mixer_test.py
@@ -7,13 +7,13 @@ from src.python.vega.utils.mixer.audiomix import AudioMix
 
 class TestAudioMixer(AudioMix):
     def __init__(self):
-        super().__init__("/home/inkoops/Temp/1-sept-2023")
+        super().__init__("1-sept-2023")
 
 
 class AudioMixerTest(unittest.TestCase):
     def test_functionality(self):
         test_audio = TestAudioMixer()
         test_audio._mix(
-            "/home/inkoops/Downloads/Untitled1.wav",
-            "/home/inkoops/Downloads/Untitled.wav",
+            "Untitled1.wav",
+            "Untitled.wav",
             test_audio._generate_filepath())
